4cab46b18006a2a90126232d8e944603c2eb8f4b
kareem444 <kareem963221@gmail.com>
2020-09-26 02:26:57 +0200

    Updates

---
+++ precache-manifest.e900288beb2752b2faa25fbc13a4aa4a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30b533e879c57bdbafdca5533b6dbb63",
+    "url": "/tagreeb/index.html"
+  },
+  {
+    "revision": "87b2ac7010295c8d253d",
+    "url": "/tagreeb/static/css/main.00bee253.chunk.css"
+  },
+  {
+    "revision": "1f782e5b0e3fd3c4d753",
+    "url": "/tagreeb/static/js/2.d73db460.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/tagreeb/static/js/2.d73db460.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87b2ac7010295c8d253d",
+    "url": "/tagreeb/static/js/main.4a9d54d0.chunk.js"
+  },
+  {
+    "revision": "e39dedca4df11d6c55f2",
+    "url": "/tagreeb/static/js/runtime-main.670d5eaf.js"
+  },
+  {
+    "revision": "6b1d7b9249e7cdf3d86b567d3032115a",
+    "url": "/tagreeb/static/media/home-bg.6b1d7b92.jpg"
+  }
+]);
